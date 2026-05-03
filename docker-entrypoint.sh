da12ce01c76b882faeb2ede8b789078e430fc9ce
Brent Marcelo <ralphbrentm@gmail.com>
2026-05-03 16:16:05 +0800

    add Tailscale for remote access

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Create a temporary .env file for Prisma to read
-# We dump the relevant environment variables into it to satisfy any
-# Prisma/NextAuth configuration that specifically looks for a .env file.
-echo "DATABASE_URL=\"$DATABASE_URL\"" > .env
-echo "NEXTAUTH_URL=\"$NEXTAUTH_URL\"" >> .env
-echo "NEXTAUTH_SECRET=\"$NEXTAUTH_SECRET\"" >> .env
-echo "GOOGLE_CLIENT_ID=\"$GOOGLE_CLIENT_ID\"" >> .env
-echo "GOOGLE_CLIENT_SECRET=\"$GOOGLE_CLIENT_SECRET\"" >> .env
-
 echo "Waiting for database to be ready..."
 sleep 5
 
